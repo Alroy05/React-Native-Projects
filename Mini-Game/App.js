c12502926909dff5339828de8ca80fed1c7bc88c
Alroy <alroypereira05@gmail.com>
2024-03-23 13:28:53 +0530

    Completed the entire game

--- Mini-Game/App.js
+++ Mini-Game/App.js
@@ -11,6 +11,8 @@ import AppLoading from 'expo-app-loading';
 export default function App() {
   const [ userNumber, setUserNumber ] = useState();
   const [ gameIsOver, setGameIsOver ] = useState(true);
+  const [ guessRounds, setGuessRounds ] = useState(0);
+  
 
   const [fontsLoaded] = useFonts({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
@@ -26,8 +28,14 @@ export default function App() {
     setGameIsOver(false);
   }
   
-  function gameOverHandler() {
+  function gameOverHandler(numberOfRounds) {
     setGameIsOver(true);
+    setGuessRounds(numberOfRounds);
+  }
+
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
   }
 
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
@@ -37,7 +45,7 @@ export default function App() {
   }
 
   if(gameIsOver && userNumber) {
-    screen = <GameOverScreen />
+    screen = <GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onStartNewGame={startNewGameHandler}/>
   }
 
   return (
